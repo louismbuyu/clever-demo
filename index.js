c1e62c182f09510a32160a40b3bc042a6480bf68
Louis Mbuyu <louismbuyu17@gmail.com>
2020-06-04 09:53:00 -0500

    added logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
 const requireHTTPS = (req, res, next) => {
     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
@@ -9,6 +11,14 @@ const requireHTTPS = (req, res, next) => {
 };
 
 app.use(requireHTTPS);
+
+const dev = app.get('env') !== 'production';
+if (!dev) {
+    app.use(morgan('common'));
+} else {
+    app.use(cors());
+    app.use(morgan('dev'));
+}
 
 app.get("/", (req, res) => {
     return res.send({message: "Hello World this is it!!! 2222 insane fast jhjkhhj"});
