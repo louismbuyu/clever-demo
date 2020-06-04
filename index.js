b7e6f24a37d838a68babf740bcc905e330e7e1dc
Louis Mbuyu <louismbuyu17@gmail.com>
2020-06-04 05:08:39 -0500

    woah

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.send({message: "Hello World this is it!!! 2222"});
+    return res.send({message: "Hello World this is it!!! 2222 insane fast"});
 });
 
 const PORT = process.env.PORT || 5000;
